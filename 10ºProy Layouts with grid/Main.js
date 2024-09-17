3e352a0eb0a5692b7dd211de28439572cb960786
Victor <rubinho_2008@hotmail.es>
2024-09-17 19:52:48 +0200

    17-09-2024

---
+++ 10ºProy Layouts with grid/Main.js
@@ -0,0 +1,10 @@
+const Boton = document.querySelector("#button");
+const layout1 = document.querySelector("#task-1");
+
+Boton.addEventListener("click", cambia-Layout);
+
+function cambiaLayout (){
+
+        Boton.classList.toggle("task-1");
+};
+
